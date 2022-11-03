da2efbf59e0d8c81da53d9d54319ed0074b12853
specialdoom <bledea.bogdan97@gmail.com>
2022-11-03 11:13:56 +0200

    Update imports

--- src/lib/components/avatars/index.js
+++ src/lib/components/avatars/index.js
@@ -1,14 +1,14 @@
-import blackcatSvelte from "./blackcat.svelte";
-import bonesSvelte from "./bones.svelte";
-import candySvelte from "./candy.svelte";
-import coffinSvelte from "./coffin.svelte";
-import evilpumpkinSvelte from "./evilpumpkin.svelte";
-import ghostSvelte from "./ghost.svelte";
-import potionSvelte from "./potion.svelte";
-import potionpotSvelte from "./potionpot.svelte";
-import skullSvelte from "./skull.svelte";
-import spiderSvelte from "./spider.svelte";
-import tombstoneSvelte from "./tombstone.svelte";
+import blackcatSvelte from "./BlackCat.svelte";
+import bonesSvelte from "./Bones.svelte";
+import candySvelte from "./Candy.svelte";
+import coffinSvelte from "./Coffin.svelte";
+import evilpumpkinSvelte from "./EvilPumpkin.svelte";
+import ghostSvelte from "./Ghost.svelte";
+import potionSvelte from "./Potion.svelte";
+import potionpotSvelte from "./PotionPot.svelte";
+import skullSvelte from "./Skull.svelte";
+import spiderSvelte from "./Spider.svelte";
+import tombstoneSvelte from "./Tombstone.svelte";
 import WhitchHatSvelte from "./WhitchHat.svelte";
 
 export default [blackcatSvelte, bonesSvelte, candySvelte, coffinSvelte, ghostSvelte, evilpumpkinSvelte, potionSvelte, skullSvelte, potionpotSvelte, spiderSvelte, tombstoneSvelte, WhitchHatSvelte]
