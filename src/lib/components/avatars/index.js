115d6540109f03e36f3d3f921d564edfa60e6bb2
specialdoom <bledea.bogdan97@gmail.com>
2022-11-03 11:05:17 +0200

    final touch

---
+++ src/lib/components/avatars/index.js
@@ -0,0 +1,14 @@
+import blackcatSvelte from "./blackcat.svelte";
+import bonesSvelte from "./bones.svelte";
+import candySvelte from "./candy.svelte";
+import coffinSvelte from "./coffin.svelte";
+import evilpumpkinSvelte from "./evilpumpkin.svelte";
+import ghostSvelte from "./ghost.svelte";
+import potionSvelte from "./potion.svelte";
+import potionpotSvelte from "./potionpot.svelte";
+import skullSvelte from "./skull.svelte";
+import spiderSvelte from "./spider.svelte";
+import tombstoneSvelte from "./tombstone.svelte";
+import WhitchHatSvelte from "./WhitchHat.svelte";
+
+export default [blackcatSvelte, bonesSvelte, candySvelte, coffinSvelte, ghostSvelte, evilpumpkinSvelte, potionSvelte, skullSvelte, potionpotSvelte, spiderSvelte, tombstoneSvelte, WhitchHatSvelte]
