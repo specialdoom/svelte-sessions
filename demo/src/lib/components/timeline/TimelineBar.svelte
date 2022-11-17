f81efb349e123944186cc3780fdd58e4e538fe56
BB <41910815+specialdoom@users.noreply.github.com>
2022-11-17 08:04:38 +0000

    chore: update timeline-bar

--- demo/src/lib/components/timeline/TimelineBar.svelte
+++ demo/src/lib/components/timeline/TimelineBar.svelte
@@ -1,21 +1,57 @@
 <script>
-  import { TextInput } from "@specialdoom/proi-ui";
+  import { TextInput, Button } from "@specialdoom/proi-ui";
+  import dayjs from "dayjs";
+  import ArrowLeftIcon from "../../icons/ArrowLeftIcon.svelte";
+  import ArrowRightIcon from "../../icons/ArrowRightIcon.svelte";
 
-  const today = Date.now();
+  const date = dayjs().format("MMMM, D");
+  const dayOfTheWeek = dayjs().format("dddd");
 </script>
 
-<div>
-  <div>{today.toFixed()}</div>
-  <TextInput placeholder="search" />
+<div class:timeline-bar={true}>
+  <div class:timeline-bar-header={true}>
+    <div class:date-and-controls={true}>
+      <span class:date={true}>{date}</span>
+      <Button variant="ghost">
+        <ArrowLeftIcon />
+      </Button>
+      <Button variant="ghost">
+        <ArrowRightIcon />
+      </Button>
+    </div>
+    <TextInput placeholder="Search task" />
+  </div>
+  <span class:day-of-the-week={true}>{dayOfTheWeek}</span>
 </div>
 
 <style>
-  div {
-    display: inline-flex;
-    justify-content: space-between;
-    align-items: center;
+  div.timeline-bar {
+    display: flex;
+    flex-direction: column;
     width: 100%;
     padding: 16px 8px;
     box-sizing: border-box;
+  }
+
+  div.timeline-bar-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 16px;
+  }
+
+  div.date-and-controls {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+  }
+
+  span.date {
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  span.day-of-the-week {
+    font-size: 12px;
   }
 </style>
