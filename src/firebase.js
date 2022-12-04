e3493f81ddb46b8c6d2bf3fa1481a6b11a7adea7
specialdoom <bledea.bogdan97@gmail.com>
2022-12-04 11:18:19 +0200

    add messaging

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 import { firebaseConfig } from "../firebase-config";
 import { getFirestore } from "firebase/firestore";
+import { getMessaging } from "firebase/messaging";
 
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
@@ -12,3 +13,5 @@ export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
 
 export const firestore = getFirestore(firebaseApp);
+
+export const firebaseMessaging = getMessaging(firebaseApp);
