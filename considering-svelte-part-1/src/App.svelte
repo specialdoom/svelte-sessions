6b8e8b9913ed7964acafdc50510a396b2b264033
specialdoom <bledea.bogdan97@gmail.com>
2023-04-09 18:01:34 +0300

    chore: add considering-svelte-part-1 folder

---
+++ considering-svelte-part-1/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Todos from "./lib/Todos.svelte";
+</script>
+
+<main>
+  <h1>Todos</h1>
+
+  <div class="card">
+    <Todos />
+  </div>
+</main>
---
+++ considering-svelte-part-1/src/lib/Todos.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { Alert, TextInput, Button } from "@specialdoom/proi-ui";
+
+  export let todos = [];
+
+  let todo = "";
+
+  function addTodo() {
+    const value = todo;
+
+    todos = [...todos, value];
+
+    todo = "";
+  }
+
+  function onKeyDown(e) {
+    if (e.key === "Enter" || e.keyCode === 13) {
+      addTodo();
+    }
+  }
+</script>
+
+<div class:todos={true}>
+  {#each todos as todo}
+    <Alert title={todo} closable />
+  {:else}
+    No todos
+  {/each}
+</div>
+<div class:card={true} style:display="inline-flex" style:gap="4px">
+  <TextInput bind:value={todo} on:keydown={onKeyDown} />
+  <Button on:click={addTodo}>Add</Button>
+</div>
+
+<style>
+  .todos {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    max-height: 270px;
+    overflow-y: auto;
+  }
+</style>
