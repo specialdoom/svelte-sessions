4b61bc04db3524679a0160b71dc4e872af80508f
specialdoom <bledea.bogdan97@gmail.com>
2023-04-22 13:52:22 +0300

    chore: update part 2

--- considering-svelte-part-2/src/App.svelte
+++ considering-svelte-part-2/src/App.svelte
@@ -2,10 +2,16 @@
   import { Tabs } from "@specialdoom/proi-ui";
   import Todos from "./lib/Todos.svelte";
   import CompletedTodos from "./lib/CompletedTodos.svelte";
+  import { getContext } from "svelte";
+
+  export let title = "Todos";
+
+  const ctx = getContext("cs-api");
 </script>
 
 <main>
-  <h1>Todos</h1>
+  <h1>{title}</h1>
+  <h3>{ctx}</h3>
 
   <Tabs>
     <Tabs.Pane index={0} title="Todos"><Todos /></Tabs.Pane>
--- considering-svelte-part-2/src/main.js
+++ considering-svelte-part-2/src/main.js
@@ -2,8 +2,15 @@ import './app.css'
 import App from './App.svelte'
 import "@specialdoom/proi-ui/variables.css";
 
+const context = new Map();
+context.set("cs-api", "todo app");
+
 const app = new App({
   target: document.getElementById('app'),
+  context,
+  props: {
+    title: "Todos from Component API"
+  }
 })
 
 export default app
